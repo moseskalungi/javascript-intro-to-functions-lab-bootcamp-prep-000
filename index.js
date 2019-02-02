1a9b2c6b684755913ce9b20be25b935c5f9023df
Moses Kalungi Sebunya <moseskalungi@gmail.com>
2019-02-02 10:27:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,14 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  return 'I can\'t hear you!'
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
 }
 
-function sayHiToGrandma(string) {
-  return "!"
-}
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
